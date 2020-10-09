55f254d92ec34c7969bcfc188b04cb6bd6c16669
Aamer Desai <desaiaamer@gmail.com>
2020-10-09 01:33:00 -0400

    Updates

---
+++ precache-manifest.783ea66f8c010fc92beef55e0dd4dfad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293483ea56ccdb2c41f7332654ae9a4b",
+    "url": "/fcc-markdown-previewer/index.html"
+  },
+  {
+    "revision": "2bc50ce8c8ce51c9f363",
+    "url": "/fcc-markdown-previewer/static/css/main.bcefbdd7.chunk.css"
+  },
+  {
+    "revision": "747156b930cff2f2a3a2",
+    "url": "/fcc-markdown-previewer/static/js/2.a4e3f6ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-markdown-previewer/static/js/2.a4e3f6ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc50ce8c8ce51c9f363",
+    "url": "/fcc-markdown-previewer/static/js/main.3ab40be1.chunk.js"
+  },
+  {
+    "revision": "53b2abfe119e19b7090d",
+    "url": "/fcc-markdown-previewer/static/js/runtime-main.4cde48bf.js"
+  }
+]);
